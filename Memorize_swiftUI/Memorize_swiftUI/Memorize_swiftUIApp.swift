75a7d25520465803210e44dacaf510bee731efe4
Ilya Dolgov <dolgov.ilya.valerievich@gmail.com>
2024-07-10 16:00:04 +0300

    Lesson 1

---
+++ Memorize_swiftUI/Memorize_swiftUI/Memorize_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_swiftUIApp.swift
+//  Memorize_swiftUI
+//
+//  Created by Ilya.Dolgov on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memorize_swiftUI/Memorize_swiftUIUITests/Memorize_swiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Memorize_swiftUIUITestsLaunchTests.swift
+//  Memorize_swiftUIUITests
+//
+//  Created by Ilya.Dolgov on 10.07.2024.
+//
+
+import XCTest
+
+final class Memorize_swiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
